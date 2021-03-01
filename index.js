cafed4c21b9d6899216a3d7a243b8d607d411ba2
Tammy <tammycwong@gmail.com>
2021-03-01 13:43:13 -0500

    Finished assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+const employee = {};
+function updateEmployeeWithKeyAndValue(employee,key,value) {
+    const cloneOfEmployee = {...employee};
+    cloneOfEmployee[key] = value;
+    return cloneOfEmployee; 
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value) {
+    employee[key] = value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee,key) {
+    const deleteEmployee = {...employee};
+    delete deleteEmployee[key];
+    return deleteEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey (employee,key) {
+    delete employee[key];
+    return employee;
+}
